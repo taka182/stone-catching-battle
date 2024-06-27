84a42edf8012327fe4701ee3dbe2ee1342c2ac9d
[taka182] <[soutakaoka92@gmail.com]>
2024-06-27 16:48:05 +0900

    add: Game画面からLose画面に遷移するnavigationを追加。Lose画面からHome画面へ遷移するnavigationを追加

--- app/src/main/java/com/example/stone_catching_battle/MainActivity.kt
+++ app/src/main/java/com/example/stone_catching_battle/MainActivity.kt
@@ -1,35 +1,44 @@
 package com.example.stone_catching_battle
 
-import android.net.wifi.hotspot2.pps.HomeSp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.stone_catching_battle.ui.theme.Stone_catching_battleTheme
 import com.example.stone_catching_battle.view.GameScreen
 import com.example.stone_catching_battle.view.HomeScreen
+import com.example.stone_catching_battle.view.LoseScreen
+import com.example.stone_catching_battle.viewmodel.GameViewModel
+import com.example.stone_catching_battle.viewmodel.HomeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+            val gameViewModel: GameViewModel = viewModel()
+
             NavHost(navController = navController, startDestination = "HomeScreen") {
                 composable(route = "HomeScreen") {
-                    HomeScreen(onStartGame = { navController.navigate("GameScreen")})
+                    HomeScreen { targetValue, playerCount ->
+                        gameViewModel.setGamePlayerCount(playerCount)
+                        gameViewModel.setGameTargetNumber(targetValue)
+                        navController.navigate("GameScreen")
+                    }
                 }
 
                 composable(route = "GameScreen") {
-                    GameScreen(onClickButton = { navController.navigate("HomeScreen")})
+                    GameScreen(gameViewModel) {
+                        navController.navigate("LoseScreen")
+                    }
+                }
+
+                composable(route = "LoseScreen") {
+                    LoseScreen {
+                        navController.navigate("HomeScreen")
+                    }
                 }
             }
         }
